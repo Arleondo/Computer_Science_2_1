8c1c4068b9bac87fe388af778dfda624b38bc65d
Arleondo2 <arnolsmith.tacca@ucsp.edu.pe>
2024-03-18 22:04:39 -0500

    Final semi work
    Added logic behind the calendar, and bug fixed a trouble that make always the calendar_runner funtion to dont add properly

--- Calendario/Imprimir_calendario_tools.h
+++ Calendario/Imprimir_calendario_tools.h
@@ -2,6 +2,19 @@
 #define CALENDARIO_IMPRIMIR_CALENDARIO_TOOLS_H
 #include <iostream>
 using namespace std;
+
+//Referencias a dias para un mejor calculo
+/* No usado (referencia para optimizar el proceso)
+string Dia_2000(){
+    return "Sabado";
+}
+
+ string Dia_1900 (){
+    return "Lunes";
+}
+
+*/
+
 //para hacer tabs en el swicth del dia
 void loop_cout_tabs(int veces){
     int y=0;
@@ -43,59 +56,56 @@ void Imprimir_mes(int Dias,int& contador){
     }
 }
 
-//Para recorrer los dias necesarios
-int calendario_runner (int dia,int mes,int year){
-    int contador_dias=0,anios=year-1900,meses=1;
-    for (;anios<year;anios++){
-        if (Bisiesto(anios)){
-            contador_dias=contador_dias+366;
-        } else {
-            contador_dias=contador_dias+365;
-        }
-    }
-    for (;meses<mes;meses++){
-        if (meses%2==1||meses==8){
-            contador_dias=+31;
-        }else if (meses%2==0&&meses!=2){
-            contador_dias=+30;
-        } else if (meses==2&&!Bisiesto(anios)){
-            contador_dias=+28;
-        }else {
-            contador_dias=+29;
-        }
-    }
-    for (int dias=1;dias<dia;dias++){
-        contador_dias++;
-    }
-    return contador_dias;
-}
-void CALENDARIO_Finder (){
-    //hay que saber que El 1/1/1900 es Lunes
-
-}
-
 //Convertidor de escrito a numero del 0-6
-int convertor_Dia_inicio(string& Dia){
+int convertor_Dia_inicio(const string& Dia){
     string Dias[7]={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
     int x=0;
     while (true){
         if (Dias[x]==Dia){
             return x;
         }
-        else {
-            cout<<"Error en escritura usar el siguiente formato:\n"<<"Mayuscula primera letra y palabra en ES";
+        else if(x>6) {
+            cout<<"Error en escritura usar el siguiente formato:\n"<<"Mayuscula primera letra y palabra en ES"<<endl;
             return -1;
         }
     }
 }
 
-//Referencias a dias para un mejor calculo
-string Dia_1900 (){
-    return "Lunes";
+//Para recorrer los dias necesarios
+long int calendario_runner (int dia,int mes,int year){
+    long int contador_dias=0,anios=year-1900,meses=1;
+
+    for (int x=0;x<anios;x++){
+        if (Bisiesto(anios)){
+            contador_dias=contador_dias+366;
+        } else {
+            contador_dias=contador_dias+365;
+        }
+    }
+
+    for (;meses<mes;meses++){
+        if (meses%2==1||meses==8){
+            contador_dias=contador_dias+31;
+        }else if (meses%2==0&&meses!=2){
+            contador_dias=contador_dias+30;
+        }else if (meses==2&&!Bisiesto(anios)){
+            contador_dias=contador_dias+28;
+        }else {
+            contador_dias=contador_dias+29;
+        }
+    }
+
+    for (int y=1;y<=dia;y++){
+        contador_dias=contador_dias+1;
+    }
+    return contador_dias;
 }
 
-string Dia_2000(){
-    return "Sabado";
+string CALENDARIO_Finder (long int Contador_dias){
+    //hay que saber que El 1/1/1900 es Lunes
+    int Dia_inicio_mes=(Contador_dias%7)+1;
+    string Dias[7]={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
+    return Dias[Dia_inicio_mes];
 }
 
 #endif //CALENDARIO_IMPRIMIR_CALENDARIO_TOOLS_H
---
+++ Calendario/cmake-build-debug/CMakeFiles/Calendario.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Calendario.dir/main.cpp.obj"
+  "CMakeFiles/Calendario.dir/main.cpp.obj.d"
+  "Calendario.exe"
+  "Calendario.exe.manifest"
+  "Calendario.pdb"
+  "libCalendario.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Calendario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
