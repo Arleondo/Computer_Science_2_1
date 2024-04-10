b2d2bc8ec05155fb8b126e424e20dba447b2f3dc
Arleondo2 <arnolsmith.tacca@ucsp.edu.pe>
2024-04-09 21:47:29 -0500

    Calculator logic finished
    La logica detras del calculador (subido recienmente por que estaba en otro repo xd)

---
+++ Calculador_Mod/Calculator_mod.h
@@ -0,0 +1,14 @@
+//
+// Created by El master crack on 5/04/2024.
+//
+
+#ifndef CALCULADOR_MOD_CALCULATOR_MOD_H
+#define CALCULADOR_MOD_CALCULATOR_MOD_H
+
+
+class Calculator_mod {
+
+};
+
+
+#endif //CALCULADOR_MOD_CALCULATOR_MOD_H
