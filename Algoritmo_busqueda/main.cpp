37932b6e43a8fc1c7372cf62d468b46e52139c61
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2024-04-10 23:52:06 +0200

    Nuevas homeworks
    Tareas añadidas el 10_04

---
+++ Algoritmo_busqueda/main.cpp
@@ -0,0 +1,6 @@
+/*
+Bool bin_search(int *ini, int *fin, int *&pos, int key)
+Hacer un buscador que encuentre en que memory storage está el key.
+En otras palabras retorna pos, por eso mismo tiene que pasar como referencia
+*/
+int ç = 29134;
---
+++ Cocktail_sort/main.cpp
@@ -0,0 +1,6 @@
+/*
+hacer el cocktail_sort el cual son bubble hacia arriba y hacia abajo
+void cocktail (int * ini, int * fin)
+Hacerlo con arithmetic de punteros
+El pare se hace cuando no hay swap o ini>fin
+*/
