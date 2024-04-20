8d10de3d3a119252f1d903f2552eb32c30f59385
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2024-04-20 15:43:57 +0200

    Adicion de la tarea multiplicacion de matrices

--- Multiplicacion_Matrices/main.cpp
+++ Multiplicacion_Matrices/main.cpp
@@ -6,7 +6,7 @@ int Multiplicacion_Matrix(int (*M)[3][3]){
     int *r=**(M+2);
     for (int *p=**M ;p<**(M+1) ;p++){
         for (int *q=**(M+1) ;q<**(M+2) ;q++) {
-            if
+            if(){}
         }
     }
 }
