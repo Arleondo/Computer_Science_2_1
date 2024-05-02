bfcde2a0e6a8788c12ce3420332a36b1c245dc63
Srgmail ;) <arnolsmith.tacca@ucsp.edu.pe>
2024-05-02 05:10:55 +0200

    Some advance in reversi

--- Reversi/main.cpp
+++ Reversi/main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
 using namespace std;
 //Hacer el reversi con una matriz de 8x8
-//Solo verificar arriba,abajo,izq,der; (Diagonales para los shushas)
+//Solo verificar arriba,abajo,izq,der; (Diagonales para los cracks)
 //De preferencia hacer que solo se pueda jugar en coordenadas adyacentes
-
-
 
 void imprimir_tablero(int (*Tablero)[8]){
 
-    int *p = *Tablero;
-    int (*q)[8]= Tablero;
-    for (  ; q<=Tablero+8 ; q++){
-
-        for ( ;p < *q ;p++){
-            cout << *p << "\t";
+    char coords [8]= {'A','B','C','D','E','F','G','H'};
+    cout << "  | ";
+    for (char coord : coords){cout << coord << " | ";}
+    cout << endl ;
+    for (int i=0;i<8;i++){
+        cout << i+1 << " | ";
+        for (int j=0;j<8;j++){
+            if ( Tablero[i][j] == 0){
+                cout << " " << " | ";
+            }
+            else if ( Tablero[i][j] == 1){
+                cout << "X" << " | ";
+            }
+            else {
+                cout << "O" << " | ";
+            }
         }
-
         cout << endl;
-
     }
 
 }
@@ -35,7 +41,7 @@ int main() {
             {0,0,0,2,1,0,0,0},
             {0,0,0,0,0,0,0,0},
             {0,0,0,0,0,0,0,0},
-            {0,0,0,0,0,0,0,1},
+            {0,0,0,0,0,0,0,0},
                    };
 
     imprimir_tablero(a);
