34bf694e6019f8cd9bad1cc483e8ad72791837fb
Arleondo2 <arnolsmith.tacca@ucsp.edu.pe>
2024-06-07 22:27:37 -0500

    Matriz_Transpuesta update
    "Cambiado" la logica detras de transponer la matriz para hacerlo solo con punteros y guardados de direcion de memorias

--- Matriz_Transpuesta/main.cpp
+++ Matriz_Transpuesta/main.cpp
@@ -32,11 +32,20 @@ void Borrar_Matriz (int Filas, int ** Matriz) {
 int ** Transpuesta_Matriz (int Filas, int Columnas, int **Matriz){
 
     int ** Matriz_Return = new int *[Columnas];
-    for (int **Filas_R = Matriz_Return, Colum_M=0; Filas_R < Matriz_Return + Columnas ; Filas_R++, Colum_M++){
+    int * arr [Columnas];
+    for (int **memory_dir=Matriz,**p = arr;memory_dir< Matriz+Filas; memory_dir++,p++){
+        *p=*memory_dir;
+    }
+    for (int **Filas_R = Matriz_Return, **Filas_M = Matriz; Filas_R < Matriz_Return + Columnas ; Filas_R++){
         *Filas_R = new int [Filas];
-        for (int **Filas_M = Matriz, *Colum_R=*Filas_R  ; Filas_M<Matriz + Filas; Filas_M++, Colum_R++){
-            *Colum_R = (*Filas_M)[Colum_M];
+        for (int *Colum_R=*Filas_R ; Filas_M<Matriz + Filas ; Filas_M++, Colum_R++){
+            *Colum_R = **Filas_M;
+            (*Filas_M)++;
         }
+        Filas_M=Matriz;
+    }
+    for (int **p=arr, **arreglo=Matriz; p<arr+Filas;p++, arreglo++ ){
+        *arreglo=*p;
     }
     return Matriz_Return;
 
@@ -62,35 +71,3 @@ int main() {
     Borrar_Matriz(3,A);
     return 0;
 }
-
-
-
-
-/* Codigo no usado (pero implementado, para "Funcionar"), error de asignacion de  memoria (La lista de *Matriz_r esta en 100 pero la de**Matriz_r esta en 300).
- * int ** Matriz_Return=Crear_Matriz(Columnas, Filas, Matriz);
-    int ** Fil_r = Matriz_Return , * Col_r = *Fil_r;
-    int ** Fil=Matriz;
-    int * Col = * Fil;
-    int * re_Fil = *(Fil_r +1);
-    for (; Fil_r < Matriz_Return+Columnas ; Fil_r++ ){
-        for (; Col_r<*(Fil_r+1) ; Col++, Col_r++){
-            if ( Col>= *(Fil+1) ){ break;}
-            *Col_r = *Col;
-        }
-        Col++;
-        if ( Col >= *(Fil + 1) ){
-            Fil++;
-            Col = * Fil;
-        }
-    }
-
-    *Primera forma de "Intento" de recorrido de punteros en el heap
-
-    int ** Matriz_Return = new int *[Columnas];
-    for (int **Filas_R = Matriz_Return, Colum_M=0; Filas_R < Matriz_Return + Columnas ; Filas_R++, Colum_M++){
-        *Filas_R = new int [Filas];
-        for (int **Filas_M = Matriz, Copiador_cont=0  ; Filas_M<Matriz + Filas; Filas_M++, Copiador_cont++){
-            *((*Filas_R)+Copiador_cont) = *((*Filas_M)+Colum_M);
-        }
-    }
-*/
